49060f39dc0a745fdde7a73ffc62f867d2011796
Ricardo Nunes <ricardo.nunes@compasso.com.br>
2019-03-08 10:35:00 -0300

    Queuing through topics.

--- cities-application/src/main/java/com/cities/configuration/RabbitMQConfiguration.java
+++ cities-application/src/main/java/com/cities/configuration/RabbitMQConfiguration.java
@@ -1,16 +1,30 @@
 package com.cities.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
 
-	private static final String CLIENT = "client";
+	public static final String ROUTING_KEY = "client";
+	public static final String CLIENTS_CITIES_EXCHANGE = "clients_cities_exchange";
 
 	@Bean
 	public Queue queue() {
-		return new Queue(CLIENT, true);
+		return new Queue(ROUTING_KEY, false);
+	}
+
+	@Bean
+	public TopicExchange clientsCitiesExchange() {
+		return new TopicExchange(CLIENTS_CITIES_EXCHANGE);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange clientsCitiesExchange) {
+		return BindingBuilder.bind(queue).to(clientsCitiesExchange).with(queue.getName());
 	}
 }
--- cities-application/src/main/java/com/cities/services/RabbitMQMessageSender.java
+++ cities-application/src/main/java/com/cities/services/RabbitMQMessageSender.java
@@ -5,6 +5,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cities.configuration.RabbitMQConfiguration;
+
 @Service
 public class RabbitMQMessageSender {
 
@@ -15,6 +17,6 @@ public class RabbitMQMessageSender {
 	private Queue queue;
 
 	public void send(String value) {
-		rabbitTemplate.convertAndSend(this.queue.getName(), value);
+		rabbitTemplate.convertAndSend(RabbitMQConfiguration.CLIENTS_CITIES_EXCHANGE, this.queue.getName(), value);
 	}
 }
